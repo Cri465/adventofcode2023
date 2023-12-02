05454a67434878be2b259a5ff7178de7c7c2ca11
Chris Minns <chrism465@gmail.com>
2023-12-02 11:57:58 +0000

    day1

---
+++ day1/puzzle1and2.rb
@@ -0,0 +1,40 @@
+require 'pry'
+def raw_input
+  file = File.open('input.txt')
+  file.readlines.map do |line|
+    line.chomp
+  end
+end
+
+def puzzle1
+  input = raw_input
+  puzzle_data = input.map do |line|
+    full_line = line.gsub(/[^0-9]/, '')
+    "#{full_line[0]}#{full_line[-1]}".to_i
+  end
+  total = puzzle_data.sum
+  p total
+end
+
+def puzzle2
+  nums =
+    { "one" => "o1e",
+    "two" => "t2o",
+    "three" => "t3e",
+    "four" => "f4r",
+    "five" => "f5e",
+    "six" => "s6x",
+    "seven" => "s7n",
+    "eight" => "e8t",
+    "nine" => "n9e" }
+
+  input = raw_input
+  puzzle_data = input.map do |line|
+    full_line = line.gsub(Regexp.union(nums.keys), nums).gsub(Regexp.union(nums.keys), nums).gsub(/[^0-9]/, '')
+    "#{full_line[0]}#{full_line[-1]}".to_i
+  end
+  p puzzle_data.sum
+end
+
+# puzzle1
+puzzle2
